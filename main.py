518d5ca050c0f89a5eb1b6e5e0eb238efc848e32
456 <ryzalen@yandex.ru>
2025-12-04 17:44:09 +0400

    test commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('alena ryzhova 7')
+    print_hi('alena ryzhova 79')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
